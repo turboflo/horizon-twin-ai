78ee52eda2ee8784976b48b15b73a7ff5d6b439a
Florian Hegenbarth <florianhegenbarth@outlook.com>
2024-02-25 17:24:23 +0100

    Update fetchData function and add client initialization***

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+const { get } = require("express/lib/response");
+
 async function fetchData() {
     const projectDescription = document.getElementById('project_description').value;
     const model = document.getElementById('model').value;
@@ -25,12 +27,49 @@ async function fetchData() {
             throw new Error('Network response was not ok');
         }
 
-        const data = await response.json();
-
-        // Clearing previous results
-
         const resultsContainer = document.getElementById('results');
         resultsContainer.innerHTML = '';
+        const getResultsButton = document.getElementById('get_results');
+        getResultsButton.disabled = true;
+        getResultsButton.classList.add('opacity-50', 'cursor-not-allowed');
+        // DISABLE BUTTON
+        getResultsButton.innerHTML = `
+        <div class="flex items-center justify-center space-x-4 py-1">
+            <div class="w-3 h-3 bg-gray-100 rounded-full animate-bounce shadow-md shadow-gray-400"></div>
+            <div class="w-3 h-3 bg-gray-200 rounded-full animate-bounce200 shadow-md shadow-gray-400"></div>
+            <div class="w-3 h-3 bg-gray-300 rounded-full animate-bounce400 shadow-md shadow-gray-400"></div>
+        </div>
+        <style>
+        @keyframes bounce {
+            0%, 100% {
+            transform: translateY(-25%);
+            animation-timing-function: cubic-bezier(0.8,0,1,1);
+            }
+            50% {
+            transform: translateY(0);
+            animation-timing-function: cubic-bezier(0,0,0.2,1);
+            }
+        }
+        .animate-bounce {
+            animation: bounce 1s infinite;
+        }
+        .animate-bounce200 {
+            animation: bounce 1s infinite 200ms;
+        }
+        .animate-bounce400 {
+            animation: bounce 1s infinite 400ms;
+        }
+        </style>
+        `;
+
+        // sleep for 5 seconds
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const data = await response.json();
+        getResultsButton.disabled = false;
+        getResultsButton.classList.remove('opacity-50', 'cursor-not-allowed');
+        getResultsButton.innerHTML = 'Get Results';
+
 
         // Dynamically creating and appending elements for each result
         data.results.forEach((result, index) => {
@@ -38,7 +77,7 @@ async function fetchData() {
             resultElement.innerHTML = `
             <div class="max-w-4xl mx-auto bg-gray-800 text-white p-4 my-4 rounded-lg shadow-lg">
                 <div class="flex justify-between items-start mb-4">
-                    <h3 class="text-xl font-semibold">${result.project.title}</h3>
+                    <h3 style="width: 65%;" class="text-xl font-semibold">${result.project.title}</h3>
                     <div style="width: 25%;" class="bg-gray-700 rounded-full h-2.5">
                         <div class="bg-blue-500 h-2.5 rounded-full" style="width: ${Math.max(result.comparison.score, 5)}%;"></div>
                         <p class="text-center text-sm text-gray-500">${result.comparison.score}/100</p>
--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from horizon_twin_ai.mock_search import mock_search_and_compare
+from horizon_twin_ai.client import Client
+
+import os
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 app = FastAPI()
+client = Client(pinecone_api_key=PINECONE_API_KEY, openai_api_key=OPENAI_API_KEY)
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,6 +32,11 @@ async def query(payload: QueryPayload):
     results = mock_search_and_compare(
         project_description=payload.project_description, top_k=payload.top_k
     )
+    # results = client.search_and_compare(
+    #     project_description=payload.project_description,
+    #     model=payload.model,
+    #     top_k=payload.top_k,
+    # )
     response = {
         "project_description": payload.project_description,
         "model": payload.model,
